39f4d681d1946ed60990dcbaa7cfc243c6c04c81
Dima <dima.kovalchuk.v@gmail.com>
2018-05-15 02:04:20 +0300

    Current user

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
-import { AuthGuardService } from './../../services/auth-guard.service';
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from '../../services/token.service';
+import { AuthGuardService } from '../../services/auth-guard.service';
+
+import { User } from './../../models/user'
+
 
 @Component({
   selector: 'navbar',
@@ -8,14 +11,19 @@ import { TokenService } from '../../services/token.service';
   styleUrls: ['./navbar.component.css'],
   providers: [TokenService, AuthGuardService]
 })
-export class NavbarComponent implements OnInit {
-  public loggedIn: boolean
-  constructor(
-   private authGuard: AuthGuardService 
-  ) { }
 
-  ngOnInit() {
-    this.authGuard.authStatus.subscribe(value => this.loggedIn = value)
+export class NavbarComponent implements OnInit {
+
+  public currentUser: User;
+
+  constructor( private authGuard: AuthGuardService) {
+    this.authGuard.getUser();
+    this.authGuard.CurrentUserChange.subscribe(user => {
+      this.currentUser = user
+    })
+  }
+
+  ngOnInit(){
   }
 
 }

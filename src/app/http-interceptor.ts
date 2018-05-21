381a650b9737cdfab13c8ad1d398f80970790b21
Dima <dima.kovalchuk.v@gmail.com>
2018-05-21 23:44:55 +0300

    Dashboard drop dinner

--- src/app/http-interceptor.ts
+++ src/app/http-interceptor.ts
@@ -20,13 +20,11 @@ export class CustomHttpInterceptor implements HttpInterceptor {
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		let isLoginRegisterPage = this.router.url.includes("signin") || this.router.url.includes("signup")
-		console.log("isLoginRegisterPage", isLoginRegisterPage)
+
 		if (this.tokenService.loggedIn()) {
 			const Authorization = this.tokenService.get(); //read the token from storahe
 			req = req.clone({ headers: req.headers.set('Authorization', "Bearer " + Authorization) });
-			console.log("LoGED IN. REQUEST Authorization create")
 		} else if(!isLoginRegisterPage) {
-			console.log("redirect")
 			this.routing.signIn();
 		}
 
